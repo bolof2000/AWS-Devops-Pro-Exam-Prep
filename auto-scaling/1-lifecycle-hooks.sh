7eb86767e6e2043940c0aee8adbae39cd87e2380
Bolofinde Olusegun <bolofbaba@MacBook-Pro.local>
2020-08-23 17:45:30 -0600

    Blue Green Deployment

--- auto-scaling/1-lifecycle-hooks.sh
+++ auto-scaling/1-lifecycle-hooks.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-aws autoscaling complete-lifecycle-action --lifecycle-action-result CONTINUE --lifecycle-hook-name LaunchHook --auto-scaling-group-name demo-asg-launch-template --instance-id i-XXXXXXXXXXXX --region eu-west-1 --profile aws-devops
+aws autoscaling complete-lifecycle-action --lifecycle-action-result CONTINUE --lifecycle-hook-name LaunchHook --auto-scaling-group-name AutoScalingBolof --instance-id i-09d8eb956c34b807f  --region eu-west-1 --profile aws-devops
 
 # lifecycle token can also be used instead of --instance-id if Lambda is used for example
